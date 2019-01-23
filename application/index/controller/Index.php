903e708ce461249ef814b3005fb611cd7edd9e14
liubowei0114 <15039157606@163.com>
2019-01-23 13:40:20 +0800

    version

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,10 +1,15 @@
 <?php
 namespace app\index\controller;
 
-class Index
+use think\Controller;
+
+class Index extends Controller
 {
     public function index()
     {
         echo "<h1 style='text-align: center'>Hello World</h1>";
+     }
+     public function ceshi(){
+         echo THINK_VERSION;
      }
 }
