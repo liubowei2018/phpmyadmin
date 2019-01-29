39c5bf2f6ad75c55328bf0d1e6b1b987eef6ef6a
liubowei0114 <15039157606@163.com>
2019-01-29 17:31:46 +0800

    version

--- application/admins/controller/Index.php
+++ application/admins/controller/Index.php
@@ -9,11 +9,27 @@
 namespace app\admins\controller;
 
 
+use think\Session;
+
 class Index extends Base
 {
     public function index(){
         $webConf = getWebConfigList();
         $this->assign('webConf',$webConf);
         return $this->fetch();
+    }
+    /**
+     * 首页
+     */
+    public function index_detail(){
+
+        return $this->fetch();
+    }
+
+    //退出登录
+    public function logout(){
+        Session::delete('admin_uid');
+        Session::delete('admin_name');
+        $this->redirect('admins/Login/index');
     }
 }
