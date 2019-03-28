eebf45619206eb87942ecbb4bbfc2d242bc4471a
liubowei2018 <980790473@qq.com>
2019-03-28 15:02:26 +0800

    state

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -7,9 +7,32 @@ class Index extends Controller
 {
     public function index()
     {
-        echo "<h1 style='text-align: center'>Welcome to 荣点</h1>";
+        $data = request()->get();
+        $token="rongdian";
+        $sigArr=[$token,$data['timestamp'],$data['nonce']];
+        sort($sigArr,SORT_STRING);
+        $sigArr = implode($sigArr);
+        $enctystr = sha1($sigArr);
+        if ($data['signature'] === $enctystr) {
+            echo $data['echostr'];
+        }else{
+            return false;
+        }
+        return $this->fetch();
      }
-     public function ceshi(){
-         echo THINK_VERSION;
+
+    /**
+     * 会员注册
+     */
+     public function register(){
+         $phone = input('param.phone');
+         $code = request()->get('code');
+         $config = config('');
+         if(empty($code)){
+             $web_url = web_url_str()."Index/Index/register/phone/".$phone;
+            $curUrl = urlencode($web_url);
+            dump($curUrl);
+         }
+         return $this->fetch();
      }
 }
