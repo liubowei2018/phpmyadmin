589cdcc7680887b9c9f33be65f87ec243f5a7725
liubowei2018 <980790473@qq.com>
2019-03-28 19:38:37 +0800

    state

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,7 +1,9 @@
 <?php
 namespace app\index\controller;
 
+use app\Interactive\model\MoneyModel;
 use think\Controller;
+use think\Db;
 use think\Session;
 
 class Index extends Controller
@@ -22,21 +24,7 @@ class Index extends Controller
 
         return $this->fetch();
      }
-    /**
-     * 会员注册
-     */
-     public function register(){
-         $phone = input('param.phone');
-         $wx_unionid = Session::get('wx_unionid');
-         $wx_unionid = 1;
-         if($wx_unionid){
 
-             return $this->fetch();
-         }else{
-            $phone = (int)$phone;
-            $this->redirect('index/authorization', ['phone' => $phone]);
-         }
-     }
     /**
      * 微信授权
      */
@@ -61,6 +49,45 @@ class Index extends Controller
          $user_info = get_request($userUrl);
          $userInfoArr = json_decode($user_info,true);
          Session::set('wx_unionid',$userInfoArr['unionid']);
-         $this->redirect('index/register', ['phone' => $phone]);
+         $member = Db::name('member')->where('uuid',$userInfoArr['unionid'])->find();
+         $t_member = Db::name('member')->where('mobile',$phone)->find();
+         if(!$member){
+            //用户不存在
+            $member_data = [
+                'uuid'=>$userInfoArr['unionid'],
+                'username'=>$userInfoArr['nickname'],
+                'user_img'=>$userInfoArr['headimgurl'],
+                'sex'=>$userInfoArr['sex']?'男':'女',
+                'create_time'=>time()
+            ];
+            $member_id = Db::name('member')->insertGetId($member_data);
+            $config = $this->red_config();
+            $MoneyModel = new MoneyModel();
+            $MoneyModel->getAddInfo(['user_id' => $member_id,'red_number'=>$config['ordinary_today_hongbao_number'],'total_red_number'=>$config['ordinary_today_hongbao_number'],'today_state'=>1]);
+            if($t_member){
+                $this->save_recommender($member_id,$t_member['id'],$t_member['pid']);
+            }
+         }else{
+             if(empty($member['pid'])){
+                 if($t_member){
+                     $this->save_recommender($member['id'],$t_member['id'],$t_member['pid']);
+                 }
+             }
+         }
+     }
+
+    /**
+     * 修改推荐人
+     */
+     private function save_recommender($uid,$pid,$gid){
+            Db::name('member')->where('id',$uid)->update(['pid'=>$pid,'gid'=>$gid]);
+     }
+     private function red_config(){
+      $app_config = Db::name('privilege_config')->select();
+      $config = [];
+      foreach ($app_config as $k => $v) {
+          $config[trim($v['name'])]=$v['value'];
+      }
+        return $config;
      }
 }
